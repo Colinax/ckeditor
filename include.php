63587eca0ba0a207d14625a79548412ed3229125
Colinax <christoph.bleiweis@outlook.com>
2021-09-18 13:16:26 +0200

    remove some placeholder icons to support modern browser behavior

--- include.php
+++ include.php
@@ -139,8 +139,8 @@ if (defined('WB_FRONTEND') && WB_FRONTEND == true) {
          * This version contains the image2 plugin, to enable it, add simply image2 to the extraPlugins list
          */
         $ckeditor->config['extraPlugins']    = 'ckawesome,codemirror,textselection,wbdroplets,wbembed,wblink,wbsave,wbshybutton,autolink,lineutils,textmatch,widgetselection,syntaxhighlight';
-        $ckeditor->config['removePlugins']   = 'wsc,save,iframe,exportpdf,flash';
-        $ckeditor->config['removeButtons']   = 'Font';
+        $ckeditor->config['removePlugins']   = 'wsc,save,iframe,exportpdf,flash,pastetext,pastefromword';
+        $ckeditor->config['removeButtons']   = 'Font,Cut,Copy,Paste';
         $ckeditor->config['fontawesomePath'] = WB_URL . '/include/font-awesome/css/font-awesome.min.css';
 
         if ($toolbar) {
